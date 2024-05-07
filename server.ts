b1f17fa73fec2504e48cc371a1148c1d5868cc38
adityadhopade <adityadhopade18@gmail.com>
2024-05-07 12:58:18 +0530

    Add Config and .env file

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import app from './src/app'
+import { config } from './src/config/config';
 console.log("Server Running");
 
 const startServer = () =>{
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Listening on PORT ${port}`);
